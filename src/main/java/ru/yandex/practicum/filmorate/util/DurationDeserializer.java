a9b5ab52bd86ecc4bb69c18d2e1f8aa62c161357
AndreySmogunov <a.batman2003@yandex.ru>
2025-07-30 22:53:33 +0300

    Доработал DurationDeserializer

--- src/main/java/ru/yandex/practicum/filmorate/util/DurationDeserializer.java
+++ src/main/java/ru/yandex/practicum/filmorate/util/DurationDeserializer.java
@@ -1,10 +1,8 @@
 package ru.yandex.practicum.filmorate.util;
 
-
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonDeserializer;
-import ru.yandex.practicum.filmorate.logger.LogMethodResult;
 
 import java.io.IOException;
 import java.time.Duration;
@@ -12,18 +10,17 @@ import java.time.Duration;
 public class DurationDeserializer extends JsonDeserializer<Duration> {
 
     @Override
-    @LogMethodResult
-    public Duration deserialize(JsonParser p,
-                                DeserializationContext ctxt
-    ) throws IOException {
-        String text = p.getText();
-
+    public Duration deserialize(JsonParser p, DeserializationContext ctxt) throws IOException {
+        if (p.getValueAsString() == null) {
+            return null;
+        }
         try {
-            return Duration.ofMinutes(Long.valueOf(text));
+            // Ожидаем число минут (например, "94")
+            long minutes = Long.parseLong(p.getValueAsString());
+            return Duration.ofMinutes(minutes);
         } catch (NumberFormatException e) {
-            return Duration.parse(text);
-        } catch (Exception e) {
-            throw new IOException("Failed to parse Duration from: " + text, e);
+            // Если пришло значение в формате PT..., используем стандартный парсинг
+            return Duration.parse(p.getValueAsString());
         }
     }
 }
